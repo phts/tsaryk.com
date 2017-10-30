5d8a23813d0a3f1c7b420ba842025c0ad57eb885
Phil Tsarik <phil.tsarik@gmail.com>
2017-10-30 18:05:07 +0300

    Try to write without semicolons

--- src/index.js
+++ src/index.js
@@ -1,16 +1,18 @@
 function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
 function getRandomColorComponent() {
-  return getRandomInt(0, 256);
+  return getRandomInt(0, 256)
 }
 
 function getRandomCssColor() {
-  return `rgb(${getRandomColorComponent()}, ${getRandomColorComponent()}, ${getRandomColorComponent()})`;
+  return `rgb(${getRandomColorComponent()}, \
+              ${getRandomColorComponent()}, \
+              ${getRandomColorComponent()})`
 }
 
 document.getElementById('btnColors').addEventListener('click', function (ev) {
-  document.body.style.backgroundColor = getRandomCssColor();
-  document.body.style.color = getRandomCssColor();
-});
+  document.body.style.backgroundColor = getRandomCssColor()
+  document.body.style.color = getRandomCssColor()
+})
