2ceb62b5459062800f0e52ac0b4115beab29b784
Phil Tsarik <phil.tsarik@gmail.com>
2017-10-30 17:53:52 +0300

    Add initial draft

---
+++ src/index.js
@@ -0,0 +1,16 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getRandomColorComponent() {
+  return getRandomInt(0, 256);
+}
+
+function getRandomCssColor() {
+  return `rgb(${getRandomColorComponent()}, ${getRandomColorComponent()}, ${getRandomColorComponent()})`;
+}
+
+document.getElementById('btnColors').addEventListener('click', function (ev) {
+  document.body.style.backgroundColor = getRandomCssColor();
+  document.body.style.color = getRandomCssColor();
+});
