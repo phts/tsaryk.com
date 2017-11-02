6380ab1e88bde797519a48bf83e93cb40745fa72
Phil Tsarik <phil.tsarik@gmail.com>
2017-11-02 19:28:52 +0300

    Integrate MobX

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ My home page. And a chance to learn some new stuff.
 
 ## Technologies
 
+* [MobX](https://mobx.js.org/)
 * [Preact](https://preactjs.com/)
 * [preact-compat](https://github.com/developit/preact-compat)
 * ~~Ramda~~ [rambdax](https://selfrefactor.github.io/rambdax/#/)
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react'
+import {Provider} from 'mobx-react'
+
 import {BtnList} from './components'
+import {stores} from './stores';
 
 export class App extends React.Component<{}, {}> {
   render() {
-    return <BtnList/>
+    return <Provider {...stores}>
+      <BtnList/>
+    </Provider>
   }
 }
