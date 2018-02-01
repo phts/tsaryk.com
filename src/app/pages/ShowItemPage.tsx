1a91965faf00fccb1d692e4f7482299d96de25cf
Phil Tsarik <phil.tsarik@gmail.com>
2018-02-01 13:52:12 +0100

    Fix button font size on Show Item page

--- src/app/pages/ShowItemPage.tsx
+++ src/app/pages/ShowItemPage.tsx
@@ -5,7 +5,7 @@ import {observer, inject} from 'mobx-react'
 import {ItemId, ItemsStore} from 'app/stores/itemsStore'
 import {I18nStore} from 'app/stores/i18nStore'
 import {OpenItemStore} from 'app/stores/openItemStore'
-import {Button, BUTTON_TYPE} from 'app/components'
+import {Button, BUTTON_TYPE, DEFAULT_FONT_SIZE} from 'app/components'
 
 interface Props {
   itemId: ItemId
@@ -29,7 +29,7 @@ export class ShowItemPage extends React.PureComponent<Props> {
       <div>
         <Button
           type={BUTTON_TYPE.Action}
-          fontSize="10pt"
+          fontSize={DEFAULT_FONT_SIZE}
           onClick={this.onBack}>
           {this.props.i18nStore.labels.close}
         </Button>
