c54f98926877ef1682d196e74e638fcdb19f2a79
Phil Tsarik <phil.tsarik@gmail.com>
2018-05-10 21:26:04 +0200

    Add more user environment data to feedback emails

--- src/app/services/email.ts
+++ src/app/services/email.ts
@@ -1,12 +1,38 @@
 import * as emailjs from 'emailjs-com'
+import browserLocale from 'browser-locale'
+
+import langStore from 'app/stores/langStore'
 
 emailjs.init('user_C3T41f8F0pv1ekwT39nYw')
 
+type TemplateParams = {
+  appLang: string,
+  innerHeight: number,
+  innerWidth: number,
+  locale: string,
+  localTime: string,
+  referrer: string,
+  screenHeight: number,
+  screenWidth: number,
+  text: string,
+  userAgent: string,
+}
+
 class EmailService {
   send(text: string) {
-    return emailjs.send('tsarik.me', 'tsarik_me', {
+    const params: TemplateParams = {
+      appLang: langStore.lang,
+      innerHeight,
+      innerWidth,
+      locale: browserLocale(),
+      localTime: new Date().toString(),
+      referrer: document.referrer,
+      screenHeight: screen.height,
+      screenWidth: screen.width,
       text,
-    })
+      userAgent: navigator.userAgent,
+    }
+    return emailjs.send('tsarik.me', 'tsarik_me', params)
   }
 }
 
