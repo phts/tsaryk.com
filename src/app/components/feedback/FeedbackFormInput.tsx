132e2dc65fb6a9a20daf6fa52ba447a2f26abd24
Phil Tsarik <phil.tsarik@gmail.com>
2018-05-10 09:28:20 +0200

    Fix feedback form input color and background for non-white page background

--- src/app/components/feedback/FeedbackFormInput.tsx
+++ src/app/components/feedback/FeedbackFormInput.tsx
@@ -3,13 +3,14 @@ import styled from 'styled-components'
 import AutosizedTextInput from 'app/components/AutosizedTextInput'
 
 export default styled(AutosizedTextInput)`
+  background-color: transparent;
   border: 0 none;
+  color: inherit;
   font-family: monospace;
   margin: 0;
   padding: 0;
 
   &:disabled {
-    background-color: transparent;
     cursor: not-allowed;
   }
 
