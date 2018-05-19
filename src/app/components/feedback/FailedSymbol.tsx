c6b72c32f6f0322b0fa7402ef70c1068bc3a9373
Phil Tsarik <phil.tsarik@gmail.com>
2018-05-19 23:04:36 +0200

    Refactor and simplify FailedSymbol and SuccessSymbol to render text by css

--- src/app/components/feedback/FailedSymbol.tsx
+++ src/app/components/feedback/FailedSymbol.tsx
@@ -1,14 +1,3 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import Symbol from './Symbol'
 
-import {blinkAnimation} from 'app/helpers/css'
-
-export default () => (
-  <Span>&#10008;</Span>
-)
-
-const Span = styled.span`
-  ${blinkAnimation}
-
-  color: red;
-`
+export default Symbol({color: 'red', cssSymbol: '2718'})
--- src/app/components/feedback/SuccessSymbol.tsx
+++ src/app/components/feedback/SuccessSymbol.tsx
@@ -1,14 +1,3 @@
-import * as React from 'react'
-import styled from 'styled-components'
+import Symbol from './Symbol'
 
-import {blinkAnimation} from 'app/helpers/css'
-
-export default () => (
-  <Span>&#10004;</Span>
-)
-
-const Span = styled.span`
-  ${blinkAnimation}
-
-  color: green;
-`
+export default Symbol({color: 'green', cssSymbol: '2714'})
