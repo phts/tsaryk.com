6852591382265f0b5d20d16c79e7821daa7022b5
Phil Tsarik <phil.tsarik@gmail.com>
2018-05-20 10:45:01 +0200

    Fix type of CommonElementProps

--- src/app/helpers/types.ts
+++ src/app/helpers/types.ts
@@ -6,7 +6,7 @@ export type Hoc<WrappedComponentPropsType, ReturnComponentType> =
   (WrappedComponent: AnyComponentClass<WrappedComponentPropsType>) =>
     ReturnComponentType
 
-export type CommonElementProps<T> = React.DetailedHTMLProps<React.HTMLAttributes<T>, T>
+export type CommonElementProps<T> = React.DetailedHTMLProps<React.HTMLAttributes<T>, {}>
 export type InputElementProps = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 export type FormElementProps = React.DetailedHTMLProps<React.FormHTMLAttributes<HTMLFormElement>, HTMLFormElement>
 export type ButtonElementProps = React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, {}>
