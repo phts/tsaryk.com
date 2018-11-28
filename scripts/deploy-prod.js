9dc560fc05ae54581d69bb1fb314355cec22c708
Phil Tsaryk <phil@tsaryk.com>
2018-11-28 11:15:24 +0100

    Fix deploy-prod script to return error properly

--- scripts/deploy-prod.js
+++ scripts/deploy-prod.js
@@ -7,17 +7,26 @@ const FTP_URL = process.env.FTP_URL
 const FTP_USER = process.env.FTP_USER
 const FTP_PASSWORD = process.env.FTP_PASSWORD
 
-;(async function () {
-  const {stdout, stderr} = await exec(
-    `lftp -u ${FTP_USER},${FTP_PASSWORD} ${FTP_URL} <<END_SCRIPT
-    mirror -R --delete --delete-first dist/ .
-    quit
-    END_SCRIPT`
-  )
-  if (stdout) {
-    console.info(stdout)
+function printResult(result) {
+  if (result.stdout) {
+    console.info(result.stdout)
   }
-  if (stderr) {
-    console.error(stderr)
+  if (result.stderr) {
+    console.error(result.stderr)
+  }
+}
+
+(async function () {
+  try {
+    const result = await exec(
+      `lftp -u ${FTP_USER},${FTP_PASSWORD} ${FTP_URL} <<END_SCRIPT
+      mirror -R --delete --delete-first dist/ .
+      quit
+      END_SCRIPT`
+    )
+    printResult(result)
+  } catch (e) {
+    printResult(e)
+    process.exit(1)
   }
 })()
