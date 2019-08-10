48367d23d02ea9671e70a8d226a638d3c6ca1152
Phil Tsaryk <phil@tsaryk.com>
2019-08-10 22:03:00 +0200

    Fix unsupported props on Link

--- src/app/components/buttons/generic/GenericLink.tsx
+++ src/app/components/buttons/generic/GenericLink.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import {Link, LinkProps} from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -8,7 +9,12 @@ interface Props extends LinkProps {
   fontSize: string
 }
 
-export default styled(Link)<Props>`
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const PureLink: React.FunctionComponent<Props> = ({buttonType, fontSize, ...props}) => (
+  <Link {...props} />
+)
+
+export default styled(PureLink)<Props>`
   border-bottom: ${props => getBorder(props.buttonType)};
   color: inherit;
   font-size: ${props => props.fontSize};
