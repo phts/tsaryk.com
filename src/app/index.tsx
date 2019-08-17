64ff85b6dc3004c14a7b443245e46152a6ac0642
Phil Tsaryk <phil@tsaryk.com>
2019-08-17 22:34:13 +0200

    Move GlobalStyle into components folder

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,7 @@ import {Provider} from 'mobx-react'
 
 import Pages from 'components/pages/Pages'
 import stores from 'stores'
-import GlobalStyle from './GlobalStyle'
+import GlobalStyle from 'components/GlobalStyle'
 
 export const App: React.FunctionComponent = () => (
   <>
