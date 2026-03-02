a8a0f0ba6ad2f0dbaf6aa25b16da1dd490971282
Phil Tsaryk <phil@tsaryk.com>
2026-03-02 22:52:44 +0100

    feat(data): update copyright year

--- src/app/data/translations/en.ts
+++ src/app/data/translations/en.ts
@@ -9,7 +9,7 @@ export const en: TranslatedStrings = {
   },
   RU: {},
   '©': {
-    name: '© Phil Tsaryk, 2025',
+    name: '© Phil Tsaryk, 2026',
     description: `
       <p>This site was created and being supported by ${link('Phil Tsaryk', 'me')} in free time.</p>
       <p>Main technologies:</p>
